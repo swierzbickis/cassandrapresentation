d8872a37f815804a6b957c86a8bf1dfbb14ed730
wierzs <Sebastian.Wierzbicki@vig.pl>
2020-03-17 22:40:44 +0000

    Base Commit

---
+++ src/main/java/com/clurgo/presentation/cassandra/CassandraPresentationApplication.java
@@ -0,0 +1,12 @@
+package com.clurgo.presentation.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraPresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraPresentationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/clurgo/presentation/cassandra/CassandraPresentationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.clurgo.presentation.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraPresentationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
